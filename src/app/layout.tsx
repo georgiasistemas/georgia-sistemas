675d961204b83b1ae5f122e9328d9829a6198b65
georgiasistemas <georgiasistemas@gmail.com>
2026-01-27 12:05:18 -0300

    Estrutura base do projeto criada

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,8 @@
-export const metadata = {
-  title: "Georgia Sistemas",
-  description: "Sistema SaaS de Delivery White Label"
-};
-
-export default function RootLayout({ children }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="pt-BR">
       <body>{children}</body>
