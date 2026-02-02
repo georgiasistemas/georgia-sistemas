e75fdff0989327969b3d081568cd98ee7d4eb130
georgiasistemas <georgiasistemas@gmail.com>
2026-02-02 14:47:55 +0000

    feat: middleware protect dashboard

---
+++ middleware.ts
@@ -0,0 +1,26 @@
+import { NextResponse } from 'next/server'
+import type { NextRequest } from 'next/server'
+import { createMiddlewareClient } from '@supabase/auth-helpers-nextjs'
+
+export async function middleware(req: NextRequest) {
+  const res = NextResponse.next()
+
+  const supabase = createMiddlewareClient({ req, res })
+  const {
+    data: { session },
+  } = await supabase.auth.getSession()
+
+  const isDashboard = req.nextUrl.pathname.startsWith('/dashboard')
+
+  if (isDashboard && !session) {
+    const redirectUrl = req.nextUrl.clone()
+    redirectUrl.pathname = '/'
+    return NextResponse.redirect(redirectUrl)
+  }
+
+  return res
+}
+
+export const config = {
+  matcher: ['/dashboard/:path*'],
+}
