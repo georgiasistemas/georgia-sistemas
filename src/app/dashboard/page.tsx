4fd60f1aeb7b02fdbd7cbb899cb564386fafe042
georgiasistemas <georgiasistemas@gmail.com>
2026-03-31 17:10:34 +0000

    actual txt

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -2,11 +2,11 @@
 
 export default function Dashboard() {
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="p-10">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
 
-      <div className="bg-green-500 text-white p-6 text-xl mt-4">
-        Tailwind funcionando
+      <div className="bg-green-500 text-white p-6 mt-4 rounded-xl">
+        Tailwind funcionando AGORA
       </div>
     </div>
   );
