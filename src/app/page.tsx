129086ad8b88a38b285efbdf4e44853fa5f8f585
georgiasistemas <georgiasistemas@gmail.com>
2026-03-31 15:08:20 +0000

    teste 7

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
 import { redirect } from "next/navigation";
 
-export default function Page() {
-  redirect("/login");
+export default function Home() {
+  return (
+    <div className="bg-green-500 text-white p-10 text-2xl">
+      TESTE TAILWIND
+    </div>
+  );
 }
